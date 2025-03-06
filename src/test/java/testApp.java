37de3438797f80413bafc9b4eb86ba034c79da23
Your Name <abderrahmanerrachidygmail.com>
2025-03-06 23:24:35 +0000

    ajouter test class to Github

---
+++ src/test/java/testApp.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class testApp {
+    @Test
+    public void testMyMethod() {
+        // Replace with your actual test logic
+        assertEquals(2 + 2, 4);
+    }
+}
